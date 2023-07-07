11d3dfdcbcfafc22bf50e62469672edb91887e20
divay805 <divay958@gmail.com>
2023-07-07 14:34:42 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+let localStream;
+let remoteStream;
+let peerConnection;
+
+const servers = {
+  iceServers: [
+    { urls: "stun:stun.l.google.com:19302" },
+    { urls: "stun:stun1.l.google.com:19302" },
+    { urls: "stun:stun2.l.google.com:19302" },
+    { urls: "stun:stun3.l.google.com:19302" },
+    { urls: "stun:stun4.l.google.com:19302" },
+  ],
+};
+
+//  function ask for the video camera connection
+let init = async () => {
+  localStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false,
+  });
+  document.getElementById("user-1").srcObject = localStream;
+  createOffer();
+};
+
+// now create an offer by setting peer to peer connection
+
+let createOffer = async () => {
+  peerConnection = new RTCPeerConnection(servers);
+
+  remoteStream = new MediaStream(); // it create and empty object to bind the audio and video data;
+  document.getElementById("user-2").srcObject = remoteStream;
+
+  // add the elments of the track from the local stream
+  localStream.getTrack().forEach((track) => {
+    peerConnection.addTrack(track, localStream);
+  });
+
+  peerConnection.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+        remoteStream.addTrack(track);
+    });
+  };
+
+  let offer = await peerConnection.createOffer(); // this  will create and offer
+  await peerConnection.setLocalDescription(offer);
+  console.log("Offer:", offer);
+};
+
+init();
