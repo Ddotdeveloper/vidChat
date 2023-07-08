8d5693e728a536c785e56b2aac46dd3175c48762
divay805 <divay958@gmail.com>
2023-07-08 12:52:06 +0530

    Making the dyamic rooms

--- main.js
+++ main.js
@@ -33,7 +33,11 @@ let init = async () => {
        await channel.join();
  
       //  channel.on("peer-online",handleUserJoined);
-      channel.on('MemberJoined',handleUserJoined)
+      channel.on('MemberJoined',handleUserJoined);
+
+      channel.on('MemberLeft',handleUserLeft);
+      
+      client.on("MessageFromPeer",handleMessageFromPeer);
       
 
   localStream = await navigator.mediaDevices.getUserMedia({
@@ -42,23 +46,59 @@ let init = async () => {
   });
 
   document.getElementById("user-1").srcObject = localStream;
-  createOffer();
-
-
+ 
 };
 
-let handleUserJoined = async (MemberId) => {
-  console.log('A new user joined the channel:', MemberId)
+let handleUserLeft = (MemberId)=>{
+  document.getElementById('user-2').style.display = 'none';
 }
 
 
-// now create an offer by setting peer to peer connection
+let handleMessageFromPeer = async (message,MemberId)=>{
+  message = JSON.parse(message.text);
+  // console.log('Message:',message);
+  if(message.type === 'offer'){
+    createAnswer(MemberId, message.offer)
+}
 
-let createOffer = async () => {
+if(message.type === 'answer'){
+    addAnswer(message.answer)
+}
+
+if(message.type === 'candidate'){
+    if(peerConnection){
+        peerConnection.addIceCandidate(message.candidate)
+    }
+}
+
+}
+
+let handleUserJoined = async (MemberId) => {
+  console.log('A new user joined the channel:', MemberId)
+  createOffer(MemberId);
+}
+
+
+
+// function that create the peer connection ===>
+let createPeerConnection = async(MemberId)=>{
   peerConnection = new RTCPeerConnection(servers);
 
-  remoteStream = new MediaStream(); // it create and empty object to bind the audio and video data;
+  
+  remoteStream = new MediaStream();
   document.getElementById("user-2").srcObject = remoteStream;
+  document.getElementById("user-2").style.display = 'block';
+
+
+  if(!localStream){
+      localStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false,
+  });
+
+  document.getElementById("user-1").srcObject = localStream;
+
+  }
 
   // add the elments of the track from the local stream
   localStream.getTracks().forEach((track) => {
@@ -74,12 +114,50 @@ let createOffer = async () => {
   peerConnection.onicecandidate = async (event) => {
     if(event.candidate){
         // console.log(`New Ice candidate`,event.candidate);
+        client.sendMessageToPeer({text:JSON.stringify({'type':'candidate','candidate':event.candidate})},MemberId);
     }
   }
+  // end of function 
+}
+
+// now create an offer by setting peer to peer connection
+let createOffer = async (MemberId) => {
+ 
+
+  await createPeerConnection(MemberId); 
 
   let offer = await peerConnection.createOffer(); 
   await peerConnection.setLocalDescription(offer);
-  // console.log("Offer:", offer);
+ 
+
+  client.sendMessageToPeer({text:JSON.stringify({'type':'offer','offer':offer})},MemberId);
 };
+
+let createAnswer = async(MemberId,offer)=>{
+  await createPeerConnection(MemberId)
+
+  await peerConnection.setRemoteDescription(offer)
+
+  let answer = await peerConnection.createAnswer()
+  await peerConnection.setLocalDescription(answer)
+
+  client.sendMessageToPeer({text:JSON.stringify({'type':'answer', 'answer':answer})}, MemberId)
+
+  
+}
+
+let addAnswer = async (answer) => {
+  if(!peerConnection.currentRemoteDescription){
+      peerConnection.setRemoteDescription(answer)
+  }
+}
+
+let leaveChannel = async()=>{
+  await channel.leave();
+  await client.logout();
+}
+
+
+window.addEventListener('beforeunload', leaveChannel)
 
 init();
